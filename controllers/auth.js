e24a38de85b9a6ebd44c7c4fd6f9ab0df29b0ea1
donavonelli <donavonelli@yahoo.com>
2020-09-21 11:49:46 -0400

    Added route so user is created in the db

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,11 +8,35 @@ router.get("/register", (req,res)=>{
     res.render("auth/register")
 })
 
+//register user in DB
+router.post("/register", async (req,res)=>{
+    try{
+        const foundEmail = await db.User.findOne({email: req.body.email});
+        const foundUsername = await db.User.findOne({username: req.body.username});
+        if(foundEmail){
+            return res.send("Email already registered")
+        }
+        if(foundUsername){
+            return res.send("Username already registered")
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(req.body.password, salt)
+        req.body.password = hash
+        await db.User.create(req.body);
+        res.redirect("/myProfile")
+    } catch (err) {
+        return res.send("Internal Service Error: ", err)
+    }
+})
+
 //login form
 router.get("/login", (req,res)=>{
     res.render("auth/login")
 })
 
+//login authenication
+
+//My
 
 
 
