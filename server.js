1905af78a65b12a3259dd8a1d3fd5e00311966ba
donavonelli <donavonelli@yahoo.com>
2020-09-21 11:00:01 -0400

    Server.js set up to start accepting users (Needs session code)

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 /* External Modules */
 const express = require("express");
 const methodOverride = require("method-override");
+const session = require("express-session")
+const MongoStore = require("connect-mongo")(session)
 
 /* Internal Modules */
 const db = require("./models");
@@ -19,9 +21,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 /* Routes */
+// Home route 
 app.get("/", (req,res)=>{
     res.render("home.ejs")
 })
+//Auth route
+app.use("/", controllers.auth)
 
 
 /* Server Listener */
