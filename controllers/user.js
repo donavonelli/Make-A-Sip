9a011f2e9c3a6a974abb9a7f880b01c310287ceb
donavonelli <donavonelli@yahoo.com>
2020-09-23 15:06:08 -0400

    All Equipment Added

--- controllers/user.js
+++ controllers/user.js
@@ -31,9 +31,43 @@ router.get("/profile/createdDrinks", (req,res) => {
     }
     res.render("user/createdDrinks", context)
 })
-//Create a Drink 
+//Create a Drink page
 router.get("/profile/newDrink", (req, res) => {
     res.render("recipe/new");
 });
+
+//Create a drink post
+router.post("/recipes/new", async (req, res) => {
+    const {name, ingredients, method, equipment} = req.body
+    try {
+        const ingredient = await db.Ingredient.findOne(ingredients)
+        const equipments = await db.Equipment.findOne(equipment)
+        const newRecipe = {
+            name: name,
+            ingredients: ingredient,
+            method: method,
+            equipment: equipments,
+        }
+        console.log(newRecipe)
+        // const recipe = await db.Recipe.create()
+    } catch (error) {
+        return console.log(error)
+    }
+    /* res.redirect("/recipes"); */
+});
+// show
+router.get("/:id", (req, res) => {
+    db.Recipe.findById(req.params.id, (error, foundRecipe) => {
+        if (error) return res.send(error);
+    })
+    const context = { recipe: foundRecipe };
+    res.render("recipe/show", context);
+});
+
+
+
+
+
+
 
 module.exports = router;
