508bae5167319daefb34dfff6606189d8cce9455
koreykristine <koreykristine@gmail.com>
2020-09-23 09:53:24 -0500

    jquery to clone ingredient field

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,7 @@
 console.log("I'm working");
+
+const $button = $("#btn");
+
+$button.on("click", function(){
+    $(".ingredient").clone().appendTo(".ingredient");
+});
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const methodOverride = require("method-override");
 const session = require("express-session")
 const MongoStore = require("connect-mongo")(session)
+const path = require("path");
 
 /* Internal Modules */
 const db = require("./models");
@@ -17,8 +18,10 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 
 /* middleware */
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
 
 /* Routes */
 // Home route 
