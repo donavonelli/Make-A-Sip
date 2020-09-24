6c2eff423885594688561278c1cb519e93e04ad2
donavonelli <donavonelli@yahoo.com>
2020-09-24 11:40:51 -0400

    Connects to remote database

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectionString = "mongodb://localhost:27017/make-a-sip";
+require("dotenv").config();
+const connectionString = process.env.MONGODB_URI 
+
 
 mongoose
   .connect(connectionString, {
